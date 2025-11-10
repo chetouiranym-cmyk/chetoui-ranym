cf7d586802787d0d0de98044fc8ffdcefb3fd174
ranym <youexample.com>
2025-11-10 15:25:21 +0100

    tp5 bebug/

---
+++ tp5 bebug/exemple1.java
@@ -0,0 +1,13 @@
+public class exemple1{
+    public static void main(String[] args){
+        int a=1;
+        int b=3;
+        int s=a+b;
+        System.out.println("etape1 a="+a);
+         System.out.println("etape1 a="+b);
+          System.out.println("etape3 s="+s);
+          s=s*2;
+           System.out.println("etape4 s*2 ="+s);
+
+    }
+}
